e786c7f94e0edf10bb282f906b264d1741c21925
dsc <sd>
2022-01-18 22:49:55 -0800

    Initial commit

---
+++ app/src/main/java/com/example/simpletodo/secondActivity.kt
@@ -0,0 +1,11 @@
+package com.example.simpletodo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class secondActivity  : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
